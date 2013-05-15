e606bb5e03e2be3b71519d71ea7c586fa306bffa
David Gonzalez <david.gonzalez@liferay.com>
2013-05-15 17:05:05 -0500

    LPS-35220 updates on OrganizationSMDHTest

--- portal-impl/test/integration/com/liferay/portlet/usersadmin/lar/OrganizationStagedModelDataHandlerTest.java
+++ portal-impl/test/integration/com/liferay/portlet/usersadmin/lar/OrganizationStagedModelDataHandlerTest.java
@@ -16,17 +16,33 @@ package com.liferay.portlet.usersadmin.lar;
 
 import com.liferay.portal.kernel.test.ExecutionTestListeners;
 import com.liferay.portal.lar.BaseStagedModelDataHandlerTestCase;
+import com.liferay.portal.model.Address;
+import com.liferay.portal.model.EmailAddress;
 import com.liferay.portal.model.Group;
+import com.liferay.portal.model.OrgLabor;
 import com.liferay.portal.model.Organization;
 import com.liferay.portal.model.OrganizationConstants;
+import com.liferay.portal.model.PasswordPolicy;
+import com.liferay.portal.model.PasswordPolicyRel;
+import com.liferay.portal.model.Phone;
 import com.liferay.portal.model.StagedModel;
+import com.liferay.portal.model.Website;
+import com.liferay.portal.service.AddressLocalServiceUtil;
+import com.liferay.portal.service.EmailAddressLocalServiceUtil;
+import com.liferay.portal.service.OrgLaborLocalServiceUtil;
 import com.liferay.portal.service.OrganizationLocalServiceUtil;
+import com.liferay.portal.service.PasswordPolicyRelLocalServiceUtil;
+import com.liferay.portal.service.PhoneLocalServiceUtil;
+import com.liferay.portal.service.ServiceContext;
 import com.liferay.portal.service.ServiceTestUtil;
+import com.liferay.portal.service.WebsiteLocalServiceUtil;
 import com.liferay.portal.test.LiferayIntegrationJUnitTestRunner;
 import com.liferay.portal.test.MainServletExecutionTestListener;
 import com.liferay.portal.test.TransactionalExecutionTestListener;
 import com.liferay.portal.util.OrganizationTestUtil;
+import com.liferay.portal.util.TestPropsValues;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -82,6 +98,34 @@ public class OrganizationStagedModelDataHandlerTest
 
 		OrganizationLocalServiceUtil.updateOrganization(organization);
 
+		Address address = OrganizationTestUtil.addAddress(parentOrganization);
+		addDependentStagedModel(
+			dependentStagedModelsMap, Address.class, address);
+
+		EmailAddress emailAddress = OrganizationTestUtil.addEmailAddress(
+			parentOrganization);
+		addDependentStagedModel(
+			dependentStagedModelsMap, EmailAddress.class, emailAddress);
+
+		OrgLabor orgLabor = OrganizationTestUtil.addOrgLabor(
+			parentOrganization);
+		_dependentOrgLabors.add(orgLabor);
+
+		ServiceContext serviceContext = ServiceTestUtil.getServiceContext(
+			group.getGroupId(), TestPropsValues.getUserId());
+		PasswordPolicy passwordPolicy =
+			OrganizationTestUtil.addPasswordPolicyRel(
+				parentOrganization, serviceContext);
+		addDependentStagedModel(
+			dependentStagedModelsMap, PasswordPolicy.class, passwordPolicy);
+
+		Phone phone = OrganizationTestUtil.addPhone(parentOrganization);
+		addDependentStagedModel(dependentStagedModelsMap, Phone.class, phone);
+
+		Website website = OrganizationTestUtil.addWebsite(parentOrganization);
+		addDependentStagedModel(
+			dependentStagedModelsMap, Website.class, website);
+
 		return parentOrganization;
 	}
 
@@ -103,20 +147,104 @@ public class OrganizationStagedModelDataHandlerTest
 
 	@Override
 	protected void validateImport(
+			StagedModel stagedModel,
 			Map<String, List<StagedModel>> dependentStagedModelsMap,
 			Group group)
 		throws Exception {
 
-		List<StagedModel> dependentStagedModels = dependentStagedModelsMap.get(
-			Organization.class.getSimpleName());
+		List<StagedModel> dependentOrganizationStagedModels =
+			dependentStagedModelsMap.get(Organization.class.getSimpleName());
 
-		Assert.assertEquals(1, dependentStagedModels.size());
+		Assert.assertEquals(1, dependentOrganizationStagedModels.size());
 
-		Organization organization = (Organization)dependentStagedModels.get(0);
+		Organization organization =
+			(Organization)dependentOrganizationStagedModels.get(0);
 
 		Assert.assertNotNull(
 			OrganizationLocalServiceUtil.fetchOrganizationByUuidAndCompanyId(
 				organization.getUuid(), group.getCompanyId()));
+
+		Organization parentOrganization = (Organization)stagedModel;
+
+		// Address t<StagedModel> dependentAddressStagedModels =
+
+		LisdependentStagedModelsMap.get(Address.class.getSimpleName());
+
+		Assert.assertEquals(1, dependentAddressStagedModels.size());
+
+		Address address = (Address)dependentAddressStagedModels.get(0);
+
+		Assert.assertNotNull(
+			AddressLocalServiceUtil.fetchAddressByUuidAndCompanyId(
+				address.getUuid(), group.getCompanyId()));
+		Assert.assertEquals(
+			parentOrganization.getPrimaryKey(), address.getClassPK());
+
+		// EmailAddress t<StagedModel> dependentEmailAddressStagedModels =
+
+		LisdependentStagedModelsMap.get(EmailAddress.class.getSimpleName());
+
+		Assert.assertEquals(1, dependentEmailAddressStagedModels.size());
+		EmailAddress emailAddress =
+			(EmailAddress)dependentEmailAddressStagedModels.get(0);
+
+		Assert.assertNotNull(
+			EmailAddressLocalServiceUtil.fetchEmailAddressByUuidAndCompanyId(
+				emailAddress.getUuid(), group.getCompanyId()));
+		Assert.assertEquals(
+			parentOrganization.getPrimaryKey(), emailAddress.getClassPK());
+
+		// OrgLabor t<OrgLabor> orgLabors =
+
+		LisOrgLaborLocalServiceUtil.getOrgLabors(
+			parentOrganization.getOrganizationId());
+		Assert.assertEquals(
+			orgLabors.get(0).getOrganizationId(),
+			_dependentOrgLabors.get(0).getOrganizationId());
+
+		// PasswordPolicyRel t<StagedModel> dependentPasswordPolicyStagedModels
+
+		Lis= dependentStagedModelsMap.get(PasswordPolicy.class.getSimpleName());
+
+		Assert.assertEquals(1, dependentPasswordPolicyStagedModels.size());
+		PasswordPolicy passwordPolicy =
+			(PasswordPolicy)dependentPasswordPolicyStagedModels.get(0);
+
+		PasswordPolicyRel passwordPolicyRel =
+			PasswordPolicyRelLocalServiceUtil.fetchPasswordPolicyRel(
+				parentOrganization.getModelClassName(),
+				parentOrganization.getPrimaryKey());
+		Assert.assertEquals(
+			passwordPolicy.getPasswordPolicyId(),
+			passwordPolicyRel.getPasswordPolicyId());
+
+		// Phone t<StagedModel> dependentPhoneStagedModels =
+
+		LisdependentStagedModelsMap.get(Phone.class.getSimpleName());
+
+		Assert.assertEquals(1, dependentPhoneStagedModels.size());
+		Phone phone = (Phone)dependentPhoneStagedModels.get(0);
+
+		Assert.assertNotNull(
+			PhoneLocalServiceUtil.fetchPhoneByUuidAndCompanyId(
+				phone.getUuid(), group.getCompanyId()));
+		Assert.assertEquals(
+			parentOrganization.getPrimaryKey(), phone.getClassPK());
+
+		// Website t<StagedModel> dependentWebsiteStagedModels =
+
+		LisdependentStagedModelsMap.get(Website.class.getSimpleName());
+
+		Assert.assertEquals(1, dependentWebsiteStagedModels.size());
+		Website website = (Website)dependentWebsiteStagedModels.get(0);
+
+		Assert.assertNotNull(
+			WebsiteLocalServiceUtil.fetchWebsiteByUuidAndCompanyId(
+				website.getUuid(), group.getCompanyId()));
+		Assert.assertEquals(
+			parentOrganization.getPrimaryKey(), website.getClassPK());
 	}
+
+	private List<OrgLabor> _dependentOrgLabors = new ArrayList<OrgLabor>();
 
 }
